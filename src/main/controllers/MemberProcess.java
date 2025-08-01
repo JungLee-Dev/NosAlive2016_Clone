03c91a73d901f34c5b54a01056540c7c56825ef7
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2025-07-31 22:13:29 -0600

    Update MemberProcess.java

--- src/main/controllers/MemberProcess.java
+++ src/main/controllers/MemberProcess.java
@@ -22,13 +22,13 @@ import models.*;
 public class MemberProcess extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberProcess() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MemberProcess() {
+		super();
+	// TODO Auto-generated constructor stub
+	    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -36,7 +36,7 @@ public class MemberProcess extends HttpServlet {
 		// TODO Auto-generated method stub
 		doProcess(request, response);
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
